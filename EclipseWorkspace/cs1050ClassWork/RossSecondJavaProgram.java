b066f43a2ecaa23f46b3c769421f1443e99e1270
Raina Ross <rross2601@gmail.com>
2025-01-24 19:12:17 -0700

    Java Hello Program
    Created a java program for GE01

---
+++ EclipseWorkspace/cs1050ClassWork/RossSecondJavaProgram.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class RossSecondJavaProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello");
+	}
+
+}
