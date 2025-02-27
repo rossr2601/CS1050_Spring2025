850de29f701fb6959f58c6c463569038a8576621
Raina Ross <rross2601@gmail.com>
2025-02-27 13:48:53 -0700

    Create M03L11LoopsConditions.java

---
+++ EclipseWorkspace/cs1050ClassWork/M03L11LoopsConditions.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class M03L11LoopsConditions {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// 2.1.1 GE03
+		
+		int max = 5;
+		int sum = 0;
+		int total = 0;
+		int count = 0;
+		
+		for (int num = 0; num <= max; num++)
+			{sum = num + sum;
+			System.out.println(sum);
+			}
+
+		while (count <= max)
+		{total = count + total;
+		System.out.println(total);
+		count++;
+		
+		}
+
+		
+	}
+	
+}
