be30c8bdf879963c3610aaaa3df2385cc2303a75
Raina Ross <rross2601@gmail.com>
2025-02-28 15:59:49 -0700

    Trying new things for in M03 L11 assignment

--- EclipseWorkspace/cs1050ClassWork/M03L11LoopsConditions.java
+++ EclipseWorkspace/cs1050ClassWork/M03L11LoopsConditions.java
@@ -19,19 +19,25 @@ public class M03L11LoopsConditions {
 		int sum = 0;
 		int total = 0;
 		int count = 0;
+		int total2 = 0;
+		int count2 = 0;
 		
 		for (int num = 0; num <= max; num++)
 			{sum = num + sum;
 			System.out.println(sum);
 			}
 
+		// 2.1.3 GE03
+		
 		while (count <= max)
-		{total = count + total;
+		{
+		total = count + total;
 		System.out.println(total);
 		count++;
-		
 		}
 
+		for (int count2 = 0; total2 <= max && count2/2 == 1)
+			
 		
 	}
 	
