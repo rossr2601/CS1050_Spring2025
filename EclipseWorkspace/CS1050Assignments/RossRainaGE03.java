d2cf68a84ae3771d7fd37dee291325b9e9731fe7
Raina Ross <rross2601@gmail.com>
2025-03-13 19:22:51 -0600

    GE03 fiddling

--- EclipseWorkspace/CS1050Assignments/RossRainaGE03.java
+++ EclipseWorkspace/CS1050Assignments/RossRainaGE03.java
@@ -17,12 +17,8 @@ public class RossRainaGE03 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	
+		System.out.print("CS1050 Final Grades");
 		
-		public static  projectinfo(String[] args)
-		{
-			
-		}
 	}
 
 }
