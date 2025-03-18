47d15d6cc195628458c9aad1a3970e3876172671
Raina Ross <rross2601@gmail.com>
2025-03-17 19:27:50 -0600

    Moved around flow for getValidGrade

--- EclipseWorkspace/CS1050Assignments/RossRainaGE03.java
+++ EclipseWorkspace/CS1050Assignments/RossRainaGE03.java
@@ -21,11 +21,11 @@ public class RossRainaGE03 {
 		final double MIN_GRADE = 0;
 		final double MAX_GRADE = 105;
 		
-		System.out.print("CS1050 Final Grades");
+		System.out.println("CS1050 Final Grades");
 		
 		Scanner input = new Scanner(System.in);
 		
-		System.out.print("Enter Class Participation grade: ");
+		System.out.println("Enter Class Participation grade: ");
 		double classParticipation  =  getValidGrade(MIN_GRADE, MAX_GRADE, input);
 
 		
@@ -35,11 +35,14 @@ public class RossRainaGE03 {
 		
 		double grade = -1;
 		
-		while (grade <= 0 || grade >=105)
+		while (grade < 0 || grade > 105)
 		{
 			grade = input.nextDouble();
 			
-			System.out.println("Please input a grade between 0 and 105");
+			if (grade < 0 || grade > 105)
+			{
+				System.out.println("Please input a grade between 0 and 105");
+			}
 		}
 		return grade;
 	
