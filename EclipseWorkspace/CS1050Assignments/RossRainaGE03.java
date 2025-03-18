cde1594ffa52997ab629ef07598bc614facaadad
Raina Ross <rross2601@gmail.com>
2025-03-17 19:24:27 -0600

    Add getValidGrade
    Ensures that the grade entered for each category is between 0 and 105

--- EclipseWorkspace/CS1050Assignments/RossRainaGE03.java
+++ EclipseWorkspace/CS1050Assignments/RossRainaGE03.java
@@ -14,16 +14,35 @@ public class RossRainaGE03 {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 
+		final double MIN_GRADE = 0;
+		final double MAX_GRADE = 105;
+		
 		System.out.print("CS1050 Final Grades");
 		
+		Scanner input = new Scanner(System.in);
 		
-		double getValidGrade(int min, int max)
-		{
-			
-		}
+		System.out.print("Enter Class Participation grade: ");
+		double classParticipation  =  getValidGrade(MIN_GRADE, MAX_GRADE, input);
+
+		
 	}
 
+	public static double getValidGrade (double MIN_GRADE, double MAX_GRADE, Scanner input) {
+		
+		double grade = -1;
+		
+		while (grade <= 0 || grade >=105)
+		{
+			grade = input.nextDouble();
+			
+			System.out.println("Please input a grade between 0 and 105");
+		}
+		return grade;
+	
+	}
+	
 }
