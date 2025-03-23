29e9951bd965f86b3ded4784bae598413d4f6ecb
Raina Ross <rross2601@gmail.com>
2025-03-23 10:29:25 -0600

    Add user inputted grades

---
+++ EclipseWorkspace/optimizing_FinalGrade_Calculator/CategoryGrade.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+
+import java.util.Scanner;
+
+public class CategoryGrade {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		double MIN_GRADE = 0;
+		double MAX_GRADE = 105;
+	}
+	
+	public static double getValidGrade (double MIN_GRADE, double MAX_GRADE, Scanner input) {
+		
+		// ensuring that to start the grade is not within range
+		double grade = -1;
+		
+		while (grade < MIN_GRADE || grade > MAX_GRADE)
+		{
+			grade = input.nextDouble();
+			
+			//error message to display if outside of range
+			if (grade < MIN_GRADE || grade > MAX_GRADE)
+			{
+				System.out.println("Please input a grade between " + MIN_GRADE + "and " + MAX_GRADE);
+			}
+		}
+		return grade;
+	}
+
+}
