dfb025165afb146654871c71e37e54ea051e447b
Raina Ross <rross2601@gmail.com>
2025-04-20 17:34:27 -0600

    Athlete summary associated methods

--- EclipseWorkspace/CS1050Assignments/Ross_ProjectImplemenation01.java
+++ EclipseWorkspace/CS1050Assignments/Ross_ProjectImplemenation01.java
@@ -28,20 +28,35 @@ public class Ross_ProjectImplemenation01 {
 		double[] athHeight = new double[athleteNum];
 		double[] avgCal = new double[athleteNum];
 		double[] athBMI = new double[athleteNum];
+		String[] athBMICategory = new String[athleteNum];
 		
 		//getting generic info to be used in later calculations
 		for (int index = 0; index < athleteNum; index++)
 		{
-			System.out.print("Enter Athlete " + (index+1) + " weight: ");
+			System.out.print("\nEnter Athlete " + (index+1) + " weight(lbs): ");
 			athWeight[index] = getValidDouble(userInput);
 			System.out.print("\nEnter Athlete " + (index+1) + " age: ");
 			athAge[index] = getValidInt(userInput);
-			System.out.print("\nEnter Athlete " + (index+1) + " height: ");
+			System.out.print("\nEnter Athlete " + (index+1) + " height(in): ");
 			athHeight[index] = getValidDouble(userInput);
+			// dailyCal pops in each iteration and we don't use it for 
+			// anything else so it makes sense to immediately calc avgCal
 			double[] dailyCal = getDailyCalBurned(userInput);
 			avgCal[index] = avgCalBurned(dailyCal);
+			athBMI[index] = calcBMI(athHeight[index],athWeight[index]);
+			// I think putting these variables in now will make iterating through
+			// the arrays for the summary easier
+			athBMICategory[index] = categoryBMI(athBMI[index]);
 			
 		}
+		
+		for (int j = 0; j < athleteNum; j++)
+		{
+			athSummary(j,athAge[j],avgCal[j],athBMI[j],athBMICategory[j]);
+		}
+		
+		topAth(avgCal);
+		getUnderweightAth(athBMICategory);
 		
 	}
 	
@@ -75,7 +90,7 @@ public class Ross_ProjectImplemenation01 {
 		
 		for (int count = 0; count < 7; count++)
 		{
-			System.out.print("Enter calories burned on day " + (count+1) + ":");
+			System.out.print("\nEnter calories burned on day " + (count+1) + ":");
 			dailyCal[count] = getValidDouble(userInput);
 		}
 		return dailyCal;
@@ -91,7 +106,86 @@ public class Ross_ProjectImplemenation01 {
 		
 		double avg = sum / dailyCal.length;
 		
-		return avg;
+		double roundedAvg = Math.round(avg * 100.0) / 100.0;
+		
+		return roundedAvg;
 	}
+	
+	// no need to validate because age has been
+	public static int maxHeartRate(int athAge)
+	{
+		int maxHR = 220 - athAge;
+		return maxHR;
+	}
+	
+	// no need to validate b/c height and weight have already been previously
+	public static double calcBMI(double height, double weight)
+	{
+		double BMI = (703*weight)/(height*height);
+		double roundedBMI = Math.round(BMI * 100.0)/ 100.0;
+		return roundedBMI;
+	}
+	
+	// assigning to a new array to be called on makes more sense
+	// I know I'm kind of just doing classes but in methods but it works best
+	// in my brain
+	public static String categoryBMI(double BMI)
+	{
+		String category;
+		
+		if (BMI < 18.5)
+		{
+			category = "Underweight";
+		}
+		else if (18.5 <= BMI && BMI < 25)
+		{
+			category = "Normal";
+		}
+		else if (25 <= BMI && BMI < 30)
+		{
+			category = "Overweight";
+		}
+		else
+		{
+			category = "Obese";
+		}
+		return category;
+	}	
+	
+	public static void athSummary(int athNum, int athAge, double avgCal, double BMI, String catBMI)
+	{
+		System.out.println("\nAthlete " + (athNum +1) + " Summary");
+		// doing HR calc here cause no need to do it sooner
+		System.out.println("\n\tMax Heart Rate: " + maxHeartRate(athAge));
+		System.out.println("\tAverage Daily Calories Burned: " + avgCal);
+		System.out.print("\tBMI: " + BMI);
+		System.out.println("\tCategory: " + catBMI);
+	}
+	
+	public static void topAth(double[] avgCal)
+	{
+		double currentMax = avgCal[0];
+		for (int count = 0; count < avgCal.length; count++)
+		{
+			if(avgCal[count] > currentMax)
+			{
+				currentMax = avgCal[count];
+			}
+		}
+		
+		System.out.println("Top Athlete by average daily calories burned is: Athlete " + (currentMax+1));
+	}
+	
+	public static void getUnderweightAth(String[] BMICat)
+	{
+		for (int count = 0; count < BMICat.length; count++)
+		{
+			if (BMICat[count] == "Underweight")
+			{
+				System.out.println("Athlete " + (count+1) + " is underweight.");
+			}
+		}
+	}
+	
 
 }
